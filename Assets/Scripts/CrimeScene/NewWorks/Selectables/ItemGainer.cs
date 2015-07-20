4fb25ef9832fa58e75d7fc46fe788df37b953e24
Kyuho Lee <modulo641@gmail.com>
2015-07-20 11:10:11 +0900

    item gain

---
+++ Assets/Scripts/CrimeScene/NewWorks/Selectables/ItemGainer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace KillingGame.CrimeScene
+{
+	public class ItemGainer : MonoBehaviour, IExecutable
+	{
+		public GameObject target;
+		
+		
+		public void Execute()
+		{
+			if (target == null)
+				return;
+			CrimeManager.Instance.obtainItem(target);
+		}
+		
+		public int ReturnIndex()
+		{
+			return 4;
+		}
+		public void SetTarget(GameObject target)
+		{
+			this.target = target;
+		}		
+	}
+}
+
